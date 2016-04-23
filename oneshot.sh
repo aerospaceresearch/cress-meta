fcc9ac91cae7b45541f829d817b5874afb11eb45
dirty0621 <info@dirk-conzelmann.de>
2016-04-23 21:16:51 +0200

    LEDs and UV enhancements

--- oneshot.sh
+++ oneshot.sh
@@ -9,42 +9,42 @@ timestamp="$(date +"%s")"
 
 filename=$imagesfolder/$timestamp.jpeg
 
-SwitchOn() {
+SwitchUVOn() {
 sudo /home/pi/433Utils/RPi_utils/codesend 1361
 }
 
-SwitchOff() {
+SwitchUVOff() {
 sudo /home/pi/433Utils/RPi_utils/codesend 1364
 }
 
-# turn light on
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
-SwitchOn
+
+SwitchLEDOn() {
+gpio mode 4 out
+gpio write 4 1
+}
+
+SwitchLEDOff() {
+gpio write 4 0
+}
+
+# turn UV light off
+SwitchUVOff; SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;
+
+# turn LED light on
+SwitchLEDOn
+
 sleep 1
 
 # take picture
 fswebcam --no-banner --rotate 180 -r 1280x720 $filename
 
+# turn LED light off
+SwitchLEDOff
 
-# turn light off
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
-SwitchOff
+
+# make post request picture
+curl -F "box=1" -F "image=@$filename" https://cress.space/v1/photo/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
+
 
 # read DHT22 sensor A
 sudo /home/pi/lol_dht22/./loldht 7 | awk '/Humidity/ { print "Inside { Hum: " $3 ", Temp: " $7 " }" }'
@@ -52,7 +52,6 @@ sudo /home/pi/lol_dht22/./loldht 7 | awk '/Humidity/ { print "Inside { Hum: " $3
 # read DHT22 sensor B
 sudo /home/pi/lol_dht22/./loldht 2 | awk '/Humidity/ { print "Outside { Hum: " $3 ", Temp: " $7 " }" }'
 
-# make post request
-curl -F "box=1" -F "image=@$filename" https://cress.space/v1/photo/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
 
-
+# TODO turn UV light on or off?
+SwitchUVOn
