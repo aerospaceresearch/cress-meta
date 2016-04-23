971c2ea11c3328fefeb0ad6f9f32dccfdab8dcb2
dirty0621 <info@dirk-conzelmann.de>
2016-04-24 00:18:53 +0200

    cleanup

--- functions.sh
+++ functions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# functions used
+# functions used for cress.space
 
 
 SwitchPumpOn() {
@@ -29,4 +29,17 @@ SwitchLEDOn() {
 SwitchLEDOff() {
 /usr/local/bin/gpio write 4 0
 }
+
+PushSensorData() {
+if [ $# -eq 3 ]; then
+temperature=$1
+humidity=$2
+position=$3
+
+curl -d "box=1&sensor_type=DHT22&value_type=temperature&position=$position&unit=°C&value=$temperature" https://cress.space/v1/sensor/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
+curl -d "box=1&sensor_type=DHT22&value_type=humidity&position=$position&unit=%&value=$humidity" https://cress.space/v1/sensor/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
+fi
+}
+export -f PushSensorData
+
 
--- oneshot.sh
+++ oneshot.sh
@@ -1,39 +1,26 @@
-#!/bin/sh -x
+#!/bin/bash
 
+# include functions for cress.space
+. functions.sh
+
+# global variables
 host=10.42.27.111
 port=3000
-postfile="api/photo"
 imagesfolder=/home/pi/images
 
 timestamp="$(date +"%s")"
+curdir=/home/pi/src
 
 filename=$imagesfolder/$timestamp.jpeg
 
-SwitchUVOn() {
-sudo /home/pi/433Utils/RPi_utils/codesend 1361
-}
-
-SwitchUVOff() {
-sudo /home/pi/433Utils/RPi_utils/codesend 1364
-}
-
-
-SwitchLEDOn() {
-gpio mode 4 out
-gpio write 4 1
-}
-
-SwitchLEDOff() {
-gpio write 4 0
-}
 
 # turn UV light off
-SwitchUVOff; SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;SwitchUVOff;
+for i in $(seq 10); do SwitchUVOff; done
 
 # turn LED light on
 SwitchLEDOn
 
-sleep 1
+#sleep 1
 
 # take picture
 fswebcam --no-banner --rotate 180 -r 1280x720 $filename
@@ -41,17 +28,13 @@ fswebcam --no-banner --rotate 180 -r 1280x720 $filename
 # turn LED light off
 SwitchLEDOff
 
-
 # make post request picture
-curl -F "box=1" -F "image=@$filename" https://cress.space/v1/photo/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
+curl -s -F "box=1" -F "image=@$filename" https://cress.space/v1/photo/ --header 'Authorization: Token 448a1661a5b8a79c9f40b2c1c491cb228baba63e'
 
-
-# read DHT22 sensor A
-sudo /home/pi/lol_dht22/./loldht 7 | awk '/Humidity/ { print "Inside { Hum: " $3 ", Temp: " $7 " }" }'
-
-# read DHT22 sensor B
-sudo /home/pi/lol_dht22/./loldht 2 | awk '/Humidity/ { print "Outside { Hum: " $3 ", Temp: " $7 " }" }'
-
+# push DHT22 sensor data
+sudo /home/pi/lol_dht22/./loldht 7  | `awk '/Humidity/ { print "PushSensorData "$7 " " $3 " inside" }' `
+sudo /home/pi/lol_dht22/./loldht 2  | `awk '/Humidity/ { print "PushSensorData "$7 " " $3 " outside" }' `
 
 # TODO turn UV light on or off?
-SwitchUVOn
+for i in $(seq 10); do SwitchUVOn; done
+
